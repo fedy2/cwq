7185ee3e18fc8fb51b870a62551fb3571c231da6
Federico De Faveri <defaveri@gmail.com>
2021-07-06 22:38:58 +0100

    Improved cli descriptions

--- internal/export/cli.go
+++ internal/export/cli.go
@@ -6,9 +6,9 @@ import (
 
 type ExportCmd struct {
 	Output           string `help:"Write to file instead of stdout" short:"o" placeholder:"file"`
-	Format           string `help:"Output format: csv or json" enum:"csv,json" default:"json"`
-	Prefix           string `help:"Exports the query definitions with a name that starts with the specified prefix."`
-	CsvIncludeHeader bool   `help:"Adds an header to the CSV file. Default: true" default:"true" negatable:""`
+	Format           string `help:"Output format: csv or json. Default: json" short:"f" enum:"csv,json" default:"json"`
+	Prefix           string `help:"Export the query definitions with a name that starts with the specified prefix."`
+	CsvIncludeHeader bool   `help:"Add an header to the CSV file. Default: true" default:"true" negatable:""`
 	CsvDelimiter     rune   `help:"Field separator in the CSV file. Default: ,"  default:"," type:"character"`
 }
 
--- internal/import/cli.go
+++ internal/import/cli.go
@@ -6,8 +6,8 @@ import (
 
 type ImportCmd struct {
 	File         string `arg:"" required:"" help:"File containing the query descriptions" type:"existingfile"`
-	Format       string `help:"File format: csv or json" enum:"csv,json" default:"json"`
-	ClearIds     bool   `help:"Strips out the ids from the query definitions" default:"false"`
+	Format       string `help:"File format: csv or json. Default: json" short:"f"  enum:"csv,json" default:"json"`
+	ClearIds     bool   `help:"Strip out the ids from the query definitions" default:"false"`
 	CsvHasHeader bool   `help:"The CSV file has an header. Default: true" default:"true" negatable:""`
 	CsvDelimiter rune   `help:"Field separator in the CSV file. Default: ,"  default:"," type:"character"`
 	CsvComment   rune   `help:"Comment character in the CSV file. Default: #"  default:"#" type:"character"`
